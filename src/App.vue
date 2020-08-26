238f31f247198446590250c9d65b2c0c9bb160c5
Jack Ling <jack.ling1@det.nsw.edu.au>
2020-08-26 17:06:46 +1000

    mock api endpoint

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-            <router-link to="/teachers">Teachers</router-link>
-
+      <router-link to="/teachers">Teachers</router-link>
     </div>
     <router-view />
   </div>
